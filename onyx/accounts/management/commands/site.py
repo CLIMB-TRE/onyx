6de9ed7ef5f89c4eb9bb84c0b58baa27329fa31c
tombch <t.brier@outlook.com>
2024-02-20 18:22:59 +0000

    Updated management commands to use bullet-point formatting on indented information

--- onyx/accounts/management/commands/site.py
+++ onyx/accounts/management/commands/site.py
@@ -1,14 +1,34 @@
-from typing import Optional, List
+from typing import Optional
 from django.core.management import base
 from ...models import Site
+from .utils import manage_instance_roles, list_instances
+
+
+ROLES = [
+    "is_active",
+]
 
 
 def create_site(
     code: str,
-    description: str,
-):
+    description: Optional[str] = None,
+) -> None:
+    """
+    Create/update a site with the given code and description.
+    
+    Args:
+        code: The code of the site.
+        description: The description of the site.
+    """
+
+    defaults = {}
+
+    if description:
+        defaults["description"] = description
+
     site, created = Site.objects.update_or_create(
-        code=code.lower(), defaults={"description": description}
+        code=code.lower(),
+        defaults=defaults,
     )
 
     if created:
@@ -16,65 +36,28 @@ def create_site(
     else:
         print(f"Updated site: {site.code}")
 
-    print("\tdescription:", site.description)
+    print(f"• Description: {site.description}")
 
 
-def manage_site_roles(
-    code: str,
-    granted: Optional[List[str]],
-    revoked: Optional[List[str]],
-):
-    site = Site.objects.get(code=code)
+def list_sites():
+    """
+    Print a table of all sites, with their roles.
+    """
 
-    print("Site:", site.code)
-
-    allowed = [
-        "is_active",
-    ]
-
-    if granted:
-        roles = []
-        for role in granted:
-            if not hasattr(site, role):
-                raise Exception("Role is unknown")
-
-            if role not in allowed:
-                raise Exception("Role cannot be changed")
-
-            setattr(site, role, True)
-            roles.append(role)
-
-        site.save(update_fields=roles)
-        print("Granted access to roles:")
-        for role in roles:
-            print(role)
-
-    elif revoked:
-        roles = []
-        for role in revoked:
-            if not hasattr(site, role):
-                raise Exception("Role is unknown")
-
-            if role not in allowed:
-                raise Exception("Role cannot be changed")
-
-            setattr(site, role, False)
-            roles.append(role)
-
-        site.save(update_fields=roles)
-        print("Revoked access to roles:")
-        for role in roles:
-            print(role)
-
-    else:
-        print("Has access to roles:")
-        for role in allowed:
-            if getattr(site, role):
-                print(role)
+    list_instances(
+        [
+            {
+                "code": site.code,
+                "is_active": site.is_active,
+                "description": f"'{site.description}'",
+            }
+            for site in Site.objects.all()
+        ]
+    )
 
 
 class Command(base.BaseCommand):
-    help = "Create/manage sites."
+    help = "Manage sites."
 
     def add_arguments(self, parser):
         command = parser.add_subparsers(
@@ -84,14 +67,18 @@ class Command(base.BaseCommand):
         # CREATE A SITE
         create_parser = command.add_parser("create", help="Create a site.")
         create_parser.add_argument("code")
-        create_parser.add_argument("-d", "--description", default="")
+        create_parser.add_argument("-d", "--description")
 
         # MANAGE SITE ROLES
         roles_parser = command.add_parser("roles", help="Manage roles for a site.")
         roles_parser.add_argument("code")
-        roles_action = roles_parser.add_mutually_exclusive_group()
-        roles_action.add_argument("-g", "--grant", nargs="+")
-        roles_action.add_argument("-r", "--revoke", nargs="+")
+        roles_parser.add_argument("-g", "--grant", nargs="+")
+        roles_parser.add_argument("-r", "--revoke", nargs="+")
+
+        # LIST SITES
+        list_parser = command.add_parser(
+            "list", help="Print a table of all sites, with their roles."
+        )
 
     def handle(self, *args, **options):
         if options["command"] == "create":
@@ -100,9 +87,37 @@ class Command(base.BaseCommand):
                 description=options["description"],
             )
 
-        elif options["command"] == "roles":
-            manage_site_roles(
-                code=options["code"],
-                granted=options.get("grant"),
-                revoked=options.get("revoke"),
-            )
+        elif options["command"] == "list":
+            list_sites()
+
+        else:
+            try:
+                site = Site.objects.get(code=options["code"])
+            except Site.DoesNotExist:
+                print(f"Site with code '{options["code"]}' does not exist.")
+                exit()
+            
+            print("Site:", site.code)
+
+            if options["command"] == "roles":
+                granted, revoked = manage_instance_roles(
+                    site,
+                    ROLES,
+                    granted=options.get("grant"),
+                    revoked=options.get("revoke"),
+                )
+
+                if granted:
+                    print("Granted roles:")
+                    for role in granted:
+                        print(f"• {role}")
+                
+                if revoked:
+                    print("Revoked roles:")
+                    for role in revoked:
+                        print(f"• {role}")
+
+                if not granted and not revoked:
+                    print("Roles:")
+                    for role in ROLES:
+                        print(f"• {role}: {getattr(site, role)}")       

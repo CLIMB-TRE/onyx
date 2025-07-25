ac0f7a8561357277a1ba411309b19acceeddce58
tombch <t.brier@outlook.com>
2025-07-25 12:52:02 +0100

    Changing access log format to use NCSA Common Log Format

--- onyx/onyx.gunicorn.py
+++ onyx/onyx.gunicorn.py
@@ -8,7 +8,9 @@ chdir = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(Path(chdir).parent / ".env")
 
 wsgi_app = "onyx.wsgi"
-access_log_format = '%(t)s %(p)s %({x-forwarded-for}i)s %(u)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(M)s ms'
+access_log_format = (
+    '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(M)s'
+)
 
 bind = os.environ["GUNICORN_BIND"]
 workers = os.environ["GUNICORN_WORKERS"]

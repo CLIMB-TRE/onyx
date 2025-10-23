1717f15052ac8bce30df8ff194560243efc3710a
tombch <t.brier@outlook.com>
2025-10-23 13:30:00 +0100

    Conditional optional value group validators

--- onyx/data/models.py
+++ onyx/data/models.py
@@ -331,6 +331,9 @@ class Analysis(PrimaryRecord):
             ),
         ]
         constraints = [
+            non_futures(
+                fields=["analysis_date"],
+            ),
             conditional_value_required(
                 field="is_published",
                 value=True,
@@ -340,9 +343,6 @@ class Analysis(PrimaryRecord):
                 field="is_published",
                 value=True,
                 optional=["report", "outputs"],
-            ),
-            non_futures(
-                fields=["analysis_date"],
             ),
         ]
 
